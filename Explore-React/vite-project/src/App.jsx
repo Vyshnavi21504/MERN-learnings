b6329c164502d07728cf812ccc6ec63ba00005f2
Vyshnavi21504 <Vyshnavi21504@gmail.com>
2025-11-19 12:30:36 +0530

    learn react

--- Explore-React/vite-project/src/App.jsx
+++ Explore-React/vite-project/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import youtube from './Youtube.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(15)
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
+
 
   return (
     <>
-      
+      <h1>React course with vysh {counter}</h1>
+      <h2>Counter Value: { counter}</h2>
+      <button onClick={addValue}>Add Value</button>{" "}
+      <button onClick = {removeValue}>remove value</button>
+      <p>footer: { counter}</p>
     </>
   )
 }
